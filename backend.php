e175324d77a09208bed794749fdb7d8b4e46b148
Duncan Bristow <duncbristow@msn.com>
2016-04-29 21:37:10 -0600

    Started building php backend

--- backend.php
+++ backend.php
@@ -1,27 +1,18 @@
 <?php
-require_once("Telnet.php");
-require_once("Telnet_Exception.php");
-
-$host = array('10.215.3.81' => 'name',36330 => 'port','ab123ab123' => 'pass'); // This is an _example_ password... please don't actually use it.
-
-$telnet = new Parsonline_Network_Telnet($host['name']); //Currently hardcoded, move to a config.php array or something like that
-$telnet->setPort($host['port']);
-$telnet->setConnectionTimeout(60); //This should be more than 5 seconds, not sure what the _maximum_ should be, however
-$telnet->setPrompt(">");
-
-try {
-    $telnet->connect();
-    $telnet->write("auth " . $host['pass']);
-    if ($telnet->waitForPrompt() == "OK\n") {
-    	$telnet->write("slot-info");
-    	$response = $telnet->waitForPrompt();
-    	$output = json_decode(substr($response, 12, -4));
-    } else {
-    	die("Wrong password!");
-    }
-    $telnet->disconnect();
-} catch (Parsonline_Network_Telnet_Exception $e) {
-    // handle connection or IO error
-    $telnet->disconnect();
-    die("Error occurred: " . $e);
+function getSlots(host,pass,port) {
+	$str = ""
+	if isset($host) {
+		$str = ($str . escapeshellarg($host));
+	}
+	if isset($pass) {
+		$str = ($str . escapeshellarg($pass));
+	}
+	if isset($port) {
+		$str = ($str . escapeshellarg($port);
+	}
+	return exec('lua wrapper.lua ' . $str);
 }
+
+require("config.php"); // Contains settings and hosts
+
+?>
